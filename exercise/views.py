9823be4ea4974fa3caffa14a3c21e41498c90396
root <root@localhost.localdomain>
2020-11-04 23:06:28 +0300

    some stuff done

--- exercise/views.py
+++ exercise/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Student, Course
+from django.http import HttpResponse, Http404
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
-    get_list = Student.Object.get('student_name')
+    template = loader.get_template('exercise/index.html')
+    get_list = Student.objects.all()
     context = {
         'name': get_list,
     }
+    output = template.render(context)
+    return HttpResponse(output, request)
